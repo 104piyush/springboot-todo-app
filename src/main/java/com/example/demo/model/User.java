a8246133b573a2ef321236f6f04046df44c18f39
104piyush <mahajanpiyush494@gmail.com>
2025-06-20 18:25:36 +0530

    Configured PostgreSQL database for Render

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -4,18 +4,21 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "users") 
 @Data
 public class User {
-	
-	@Id
-	@GeneratedValue
-	private long id;
-	@Column(unique = true)
-	private String name;
-	private String email;
-	private String password;
-	
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String email;
+    private String password;
 }
